caaf40ac2c3fd3e88dc8588912ef60f4eedec364
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2023-12-20 23:26:04 +0100

    DBStorage ^^

---
+++ models/engine/db_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This module defines a class to manage - db storage - for hbnb clone """
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+class DBStorage:
+    """Create the mysql db engine"""
+    __engine = None
+    __session = None
