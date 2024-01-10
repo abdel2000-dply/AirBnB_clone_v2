27a11dd4f75d566788f7181690e5c225e69b04b6
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-01-10 20:17:18 +0100

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,7 +19,6 @@ sudo ln -s /data/web_static/releases/test /data/web_static/current
 config="/etc/nginx/sites-available/default"
 location=$(grep -Fn location $config | head -1 | cut -d":" -f1)
 hbnb_static="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-
 sudo sed -i "${location}i ${hbnb_static}" "${config}"
 
 sudo service nginx restart
