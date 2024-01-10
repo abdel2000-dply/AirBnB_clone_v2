cf93a4c2d3b1f960b2bcf8325fac8d51fd4a46ab
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-01-10 20:08:51 +0100

    task 0 :fire:

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Script to set up web servers for deployment of web_static
+
+if ! command -v nginx &> /dev/null
+then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+    sudo service nginx start
+fi
+
+sudo mkdir -p /data/web_static/{releases/test,shared}
+sudo chown -R ubuntu:ubuntu /data/
+
+echo "<html><head></head><body>Hello World!</body></html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test /data/web_static/current
+
+config="/etc/nginx/sites-available/default"
+location=$(grep -Fn location $config | head -1 | cut -d":" -f1)
+hbnb_static="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+
+sudo sed -i "${location}i ${hbnb_static}" "${config}"
+
+sudo service nginx restart
