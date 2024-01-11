1944cae096a45c227256002ffc7e65f5322e4c6c
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-01-11 17:00:45 +0100

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
-# Script to set up web servers for deployment of web_static
+# Script to set up web server for deployment
 
-sudo apt update -y
-sudo apt install nginx -y
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
+
+default_sites="/etc/nginx/sites-available/default"
+location=$(grep -Fn location $default_sites | head -1 | cut -d":" -f1)
+hbnb_static="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 
 sudo mkdir -p /data/web_static/{releases/test,shared}
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu "/data/"
+sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
 
-echo "<html><head></head><body>Hello World!</body></html>" | sudo tee /data/web_static/releases/test/index.html
+echo "<html><head></head><body>Hello World!</body></html>" | sudo tee "/data/web_static/releases/test/index.html"
 
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-config="/etc/nginx/sites-available/default"
-location=$(grep -Fn location $config | head -1 | cut -d":" -f1)
-hbnb_static="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i "${location}i ${hbnb_static}" "${config}"
-
+sudo sed -i "${location}i ${hbnb_static}" "${default_sites}"
 sudo service nginx restart
