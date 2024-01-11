7a5931cdfb40b889e08da522a919909af23f2e33
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-01-11 16:24:46 +0100

    ^^

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 # Script to set up web servers for deployment of web_static
 
-sudo apt update -y
-sudo apt install nginx -y
+apt update -y
+apt install nginx -y
 
-sudo mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"
-sudo chown -R ubuntu:ubuntu /data/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+chown -R ubuntu:ubuntu /data/
 
 echo "<html><head></head><body>Hello World!</body></html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
