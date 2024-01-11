137badddf53ea9af78f818b93286c55ed30da6eb
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-01-11 16:30:57 +0100

    testing

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
-# Script to set up web servers for deployment of web_static
+# Script to set up web server for deployment
 
-apt update -y
-apt install nginx -y
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-chown -R ubuntu:ubuntu /data/
+default_sites="/etc/nginx/sites-available/default"
+location=$(grep -Fn location $default_sites | head -1 | cut -d":" -f1)
+hbnb_static="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 
-echo "<html><head></head><body>Hello World!</body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"
+echo "<h1>Hello World!</h1>" | sudo tee "/data/web_static/releases/test/index.html"
+sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+sudo chown -hR ubuntu:ubuntu "/data/"
 
-ln -sf /data/web_static/releases/test /data/web_static/current
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo sed -i "${location}i ${hbnb_static}" "${default_sites}"
 sudo service nginx restart
