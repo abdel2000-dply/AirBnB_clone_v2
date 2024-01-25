971e8f0cce6770f2024e276db90997d6fd974a2b
abdel2000-dply <abno870@gmail.com>
2024-01-26 00:46:01 +0100

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A simple Flask web application"""
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -14,6 +15,11 @@ def hello():
 def hbnb():
     """display 'HBNB'"""
     return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """display “C ” followed by the value of the text"""
+    return 'C {}'.format(escape(text.replace('_', ' ')))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
