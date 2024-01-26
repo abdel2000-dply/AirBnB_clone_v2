0e41720f86d88ff069200fc395b7df749a5a606f
abdel2000-dply <abno870@gmail.com>
2024-01-26 01:01:43 +0100

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A simple Flask web application"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """display “C ” followed by the value of the text"""
+    return 'C {}'.format(escape(text.replace('_', ' ')))
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def Python_text(text):
+    """display 'Python' followed by the value of the text"""
+    return 'Python {}'.format(escape(text.replace('_', ' ')))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
