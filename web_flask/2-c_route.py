59c96358e8166b61a06bcf5524cdeb6d6101307c
abdel2000-dply <abno870@gmail.com>
2024-01-26 01:01:36 +0100

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,15 +11,18 @@ def hello():
     """Display 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """display 'HBNB'"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """display “C ” followed by the value of the text"""
     return 'C {}'.format(escape(text.replace('_', ' ')))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
