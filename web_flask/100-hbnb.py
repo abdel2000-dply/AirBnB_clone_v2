09b2d30ba85469b60ed32576bc5cc77c2ca69a19
abdel2000-dply <abno870@gmail.com>
2024-01-27 23:11:04 +0100

    task 12

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -9,12 +9,12 @@ from models.place import Place
 app = Flask(__name__)
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """display hbnb"""
     states = storage.all("State").values()
     amenities = storage.all("Amenity").values()
-    places = storage.all(Place).values()
+    places = storage.all("Place").values()
     return render_template(
         '100-hbnb.html', states=states, amenities=amenities, places=places
     )
