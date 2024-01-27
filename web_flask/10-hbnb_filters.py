db8633558db053756df4b7358b869cc1236a0b46
abdel2000-dply <abno870@gmail.com>
2024-01-27 20:20:24 +0100

    task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -13,8 +13,9 @@ def filters():
     """display hbnb_filters from static"""
     states = storage.all("State").values()
     amenities = storage.all("Amenity").values()
-    return render_template('10-hbnb_filters.html', states=states,
-                            amenities=amenities)
+    return render_template(
+        '10-hbnb_filters.html', states=states, amenities=amenities
+    )
 
 
 @app.teardown_appcontext
