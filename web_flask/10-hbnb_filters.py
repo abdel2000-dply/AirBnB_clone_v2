1fa245782ff276a692e3f1dec004f446c61814e2
abdel2000-dply <abno870@gmail.com>
2024-01-27 19:40:05 +0100

    :fire:

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A simple Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.amenity import Amenity
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Remove the current SQLAlchemy Session (after each request) """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
